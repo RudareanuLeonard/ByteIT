a27240bc36a7d4898376f122be67f9ef6e6ceb20
radu19 <radutodosan@yahoo.com>
2023-11-07 01:42:46 +0200

    v0.3 added routing (we can now navigate between welcome and courses page)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {CoursesComponent} from "./courses/courses.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'courses', component: CoursesComponent},
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [WelcomeComponent, CoursesComponent]
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import {NgOptimizedImage} from "@angular/common";
-import { WelcomeComponent } from './welcome/welcome.component';
 import { DescriptionComponent } from './welcome/description/description.component';
 import { FeaturesComponent } from './welcome/features/features.component';
 import { FeatureBoxComponent } from './welcome/features/feature-box/feature-box.component';
@@ -15,11 +14,11 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [
     AppComponent,
     NavBarComponent,
-    WelcomeComponent,
     DescriptionComponent,
     FeaturesComponent,
     FeatureBoxComponent,
-    FooterComponent
+    FooterComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
