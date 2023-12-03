dc637734cef774f9b04580f2e1fa6fcc96a6a7bc
radutodosan <radutodosan@gmail.com>
2023-12-03 18:29:47 +0200

    some css changes and finalised signup form structure

--- frontend/src/app/sign-up-pop-up/sign-up-pop-up.component.ts
+++ frontend/src/app/sign-up-pop-up/sign-up-pop-up.component.ts
@@ -1,12 +1,31 @@
-
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthenticationService} from "../services/authentication.service";
 
 @Component({
   selector: 'app-sign-up-pop-up',
   templateUrl: './sign-up-pop-up.component.html',
   styleUrls: ['./sign-up-pop-up.component.css']
 })
-export class SignUpPopUpComponent {
+export class SignUpPopUpComponent implements OnInit{
+  signupForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  signupUser(){
+
+  }
 
   closeModal() {
     const loginModal = document.getElementById("signupModal");
